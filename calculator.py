b8c3636b8c76fa28b8354fc709bb03e381e898d7
Jonas <jonas.soehnlein@stud.uni-regensburg.de>
2021-04-27 10:50:07 +0200

    final

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 import sys
-import datetime
 from PyQt5 import uic, QtWidgets, QtCore, QtGui
 
 
-class Counter(QtWidgets.QWidget):
+# Initialize class Calculator
+class Calculator(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.count = 0
         self.initUI()
 
+# Load interface and define all 18 calculator inputs
     def initUI(self):
         self.ui = uic.loadUi("calculator.ui", self)
         self.show()
@@ -31,137 +31,189 @@ class Counter(QtWidgets.QWidget):
         self.ui.dot.clicked.connect(self.method_dot)
         self.ui.pushButton.clicked.connect(self.method_result)
 
+    # Define all methods for text input
     def method_1(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "1")
+        print(text + "1")
 
     def method_2(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "2")
+        print(text + "2")
 
     def method_3(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "3")
+        print(text + "3")
 
     def method_4(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "4")
+        print(text + "4")
 
     def method_5(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "5")
+        print(text + "5")
 
     def method_6(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "6")
+        print(text + "6")
 
     def method_7(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "7")
+        print(text + "7")
 
     def method_8(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "8")
+        print(text + "8")
 
     def method_9(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "9")
+        print(text + "9")
 
     def method_0(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "0")
+        print(text + "0")
 
     def method_dot(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + ".")
+        print(text + ".")
 
     def method_plus(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "+")
+        print(text + "+")
 
     def method_minus(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "-")
+        print(text + "-")
 
     def method_mult(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "*")
+        print(text + "*")
 
     def method_div(self):
         text = self.output_Results.text()
         self.output_Results.setText(text + "/")
+        print(text + "/")
 
     def method_clear(self):
         self.output_Results.setText("")
+        print("")
 
     def method_delete(self):
         text = self.output_Results.text()
         self.output_Results.setText(text[:len(text) - 1])
+        print(text[:len(text) - 1])
 
     def method_result(self):
         text = self.output_Results.text()
 
         try:
-            ans=eval(text)
-            self.output_Results.setText(str(ans))
-        except:
-            self.output_Results.setText("Wrong Input")
+            result = eval(text)
+            self.output_Results.setText(str(result))
+            print(result)
+        except Exception:
+            self.output_Results.setText("This input can't be calculated")
+            print("Sorry, this input can't be calculated")
 
-    # key input
+    # Implement key inputs for all relevant buttons
     def keyPressEvent(self, event):
         if event.text() == '1':
             text = self.output_Results.text()
             self.output_Results.setText(text + "1")
+            print(text + "1")
         if event.text() == '2':
             text = self.output_Results.text()
             self.output_Results.setText(text + "2")
+            print(text + "2")
         if event.text() == '3':
             text = self.output_Results.text()
             self.output_Results.setText(text + "3")
+            print(text + "3")
         if event.text() == '4':
             text = self.output_Results.text()
             self.output_Results.setText(text + "4")
+            print(text + "4")
         if event.text() == '5':
             text = self.output_Results.text()
             self.output_Results.setText(text + "5")
+            print(text + "5")
         if event.text() == '6':
             text = self.output_Results.text()
             self.output_Results.setText(text + "6")
+            print(text + "6")
         if event.text() == '7':
             text = self.output_Results.text()
             self.output_Results.setText(text + "7")
+            print(text + "7")
         if event.text() == '8':
             text = self.output_Results.text()
             self.output_Results.setText(text + "8")
+            print(text + "8")
         if event.text() == '9':
             text = self.output_Results.text()
             self.output_Results.setText(text + "9")
+            print(text + "9")
         if event.text() == '0':
             text = self.output_Results.text()
             self.output_Results.setText(text + "0")
+            print(text + "0")
         if event.text() == '.':
             text = self.output_Results.text()
             self.output_Results.setText(text + ".")
+            print(text + ".")
         if event.text() == '+':
             text = self.output_Results.text()
             self.output_Results.setText(text + "+")
+            print(text + "+")
         if event.text() == '-':
             text = self.output_Results.text()
             self.output_Results.setText(text + "-")
+            print(text + "-")
         if event.text() == '*':
             text = self.output_Results.text()
             self.output_Results.setText(text + "*")
+            print(text + "*")
         if event.text() == '/':
             text = self.output_Results.text()
             self.output_Results.setText(text + "/")
-        #clear
-        #delete
-        #equal
+            print(text + "/")
+        if event.text() == 'd':
+            text = self.output_Results.text()
+            self.output_Results.setText(text[:len(text) - 1])
+            print(text[:len(text) - 1])
+        if event.text() == 'c':
+            self.output_Results.setText("")
+            print("")
+        if event.text() == '=':
+            text = self.output_Results.text()
+
+            try:
+                result = eval(text)
+                self.output_Results.setText(str(result))
+                print(result)
+            except Exception:
+                self.output_Results.setText("This input can't be calculated")
+                print("Sorry, this input can't be calculated")
+
 
 def main():
+
     app = QtWidgets.QApplication(sys.argv)
-    cnt = Counter()
+    cnt = Calculator()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
